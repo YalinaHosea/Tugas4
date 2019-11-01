124e51e5129748bd60f025e6784af85f8271d80a
YalinaHosea <yalinaa13@gmail.com>
2019-11-01 17:58:47 +0700

    PreferenceScreen

--- app/src/main/java/com/example/tugas4/Main.java
+++ app/src/main/java/com/example/tugas4/Main.java
@@ -2,7 +2,11 @@ package com.example.tugas4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends AppCompatActivity {
 
@@ -10,5 +14,39 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(myBtnSettingClick);
+        SharedPreferences prefs = Main.this.getSharedPreferences("prefs_file",MODE_PRIVATE);
+        String statusLogin = prefs.getString("isLogin",null); Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(myBtnLoginClick);
+        if (statusLogin != null){
+            button3.setText("Logout");
+        }else{
+            button3.setText("Login"); }
     }
+
+    private View.OnClickListener myBtnSettingClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(Main.this, SettingsActivity.class);
+            startActivity(intent);
+        }
+    };
+    private View.OnClickListener myBtnLoginClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            SharedPreferences prefs = Main.this.getSharedPreferences("prefs_file",MODE_PRIVATE);
+            String statusLogin = prefs.getString("isLogin",null);
+            SharedPreferences.Editor edit = prefs.edit();
+
+        Button btnLogin = findViewById(R.id.button3);
+        if (statusLogin != null){
+            edit.putString("isLogin",null);
+            btnLogin.setText("Login");
+        }else{
+            edit.putString("isLogin","Admin");
+            btnLogin.setText("Logout");         }
+        edit.commit();
+        }
+    };
 }
